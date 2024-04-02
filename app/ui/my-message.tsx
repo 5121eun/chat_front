9330c3f5e25230f66e66527ec2cd971d7fc421ee
5121eun <5121eun@gmail.com>
2024-04-02 19:08:44 +0900

    update message size

--- app/ui/my-message.tsx
+++ app/ui/my-message.tsx
@@ -2,11 +2,11 @@ export default function MyMessage({ message }: { message: string }) {
     return (
         <div className="chat-message">
             <div className="flex items-end justify-end">
-                <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+                <div className="flex flex-col space-y-2 text-md max-w-xs mx-2 order-1 items-end">
                     <div>
                         <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{message}</span></div>
                     </div>
-                <img src="user.png" alt="My profile" className="w-6 h-6 rounded-full order-2"/>
+                <img src="user.png" alt="My profile" className="w-10 h-10 rounded-full order-2"/>
             </div>
         </div>
     )
--- app/ui/server-message.tsx
+++ app/ui/server-message.tsx
@@ -2,10 +2,10 @@ export default function Message({ message }: { message: string }) {
     return (
         <div className="chat-message">
             <div className="flex items-end">
-                <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                <div className="flex flex-col space-y-2 text-md max-w-xs mx-2 order-2 items-start">
                 <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">{message}</span></div>
                 </div>
-                <img src="robot.png" alt="My profile" className="w-6 h-6 rounded-full order-1"/>
+                <img src="robot.png" alt="My profile" className="w-10 h-10 rounded-full order-1"/>
             </div>
         </div>
     )
